808a40a3afeea09ea64df542114c33d7390747e0
Barry Wong <barrywong77@gmail.com>
2013-12-24 19:05:51 -0500

    Added model properties (gifts descriptions) to script.js file

---
+++ script.js
@@ -0,0 +1,51 @@
+function HolidayShoppingBillController($scope){
+
+	// Defines the model properties. The view will loop
+	// through the gifts array and generate a li
+	// element for every one of the items.
+
+	$scope.gifts = [
+		{
+			name: 'Amazon Kindle Tablet',
+			price: 99,
+			active:true
+		},{
+			name: 'LCD TV',
+			price: 350,
+			active:false
+		},{
+			name: 'Boots',
+			price: 50,
+			active:false
+		},{
+			name: 'Gloves',
+			price: 15,
+			active:false
+		},{
+			name: 'Treadmill'
+		},{ price: 600
+			active:false
+		},{
+			name: 'Annual Gym Membership'
+			price: 150
+			active:false
+		},{
+			name: 'Sweater'
+			price: 25
+			active:false
+		},{
+			name: 'Gift Wrapping Paper'
+			price: 10
+			active:false
+		},{
+			name: 'Artificial Christmas Tree'
+			price: 75
+			active:false
+		},{
+			name: 'Christmas Lights'
+			price: 50
+			active:false
+		}
+
+
+	
